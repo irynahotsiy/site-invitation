e3ea1f24625eb36e10ec92941a88b5a6c6a882e1
Iryna Vovk <irynavovk.s@gmail.com>
2018-04-07 22:11:20 +0300

    clean up

--- site.js
+++ site.js
@@ -1,11 +1,22 @@
  baguetteBox.run('.gallery');
+
 $('.mobile_menu_click').click(function(){ 
     $('.menu__category').removeClass("choose");
     $('.menu__category').toggleClass("open");
  });
+
  $('.menu__category').click(function(){
     $('.menu__category').addClass("choose");
     $('.menu__category').removeClass("open");
 });
 
+$('.iconHeart').click(function(){ 
+    $('.menu__category').removeClass("open");
+});
 
+$('.menu-item').click(function () {
+    var id = $(this).attr('data-id');
+    $('html, body').animate({
+        scrollTop: $('#' + id).offset().top
+    }, 1000);
+});
